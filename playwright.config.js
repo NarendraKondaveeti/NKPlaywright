de009ad87ba5ff158962724d2b4e0ec75e1361e2
Narendra Kondaveeti (ALLYIS INC) <v-narendrako@microsoft.com>
2024-10-29 00:46:58 +0530

    restart after laptop format

--- playwright.config.js
+++ playwright.config.js
@@ -34,7 +34,7 @@ module.exports = defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     //trace: 'on',
     screnshot: 'on',
-    headless: true,
+    headless: false,
     launchOptions:{
       slowMo: 80
     }
